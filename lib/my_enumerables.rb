8b0c2c15acee37f917f6b50d41dabb8d6a6a97be
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-06-21 09:43:27 -0400

    First commit for my custom enum odin project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    idx = 0
+    self.my_each do 
+      yield(self[idx], idx) if block_given?
+      idx += 1
+    end
+    self
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +17,23 @@ end
 # to this method
 class Array
   # Define my_each here
+  # for each element in the array do the block of code
+  def my_each
+    # find the length of the array
+    # while the current iteration (start at 0?) is less than length of the array
+    # yield to the block of code
+    # end when the length of the array is equal to the current iteration
+    counter = 0
+    while counter < self.length
+      yield(self[counter]) if block_given?
+      counter += 1
+    end
+    # not sure on this self here, but the tests passed..ask peers about it
+    self
+  end
 end
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+# a.my_each_with_index { |element, idx| p "element is: #{element}. index is: #{idx}" }
+# a.my_each { |element| p "element is: #{element}." }
+
